a8c2ab6b5259fb5f7211d59ce3a18049d86866a7
Israel Okorafor <iykeprince48@gmail.com>
2023-05-03 11:47:38 +0100

    added bootstrap css cdn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/globals.css";
-import "../components/Pagination/Pagination.scss";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/globals.css'
+import '../components/Pagination/Pagination.scss'
 
-import type { AppProps } from "next/app";
-import Script from "next/script";
-import { AuthUserProvider } from "../contexts/auth.context";
-import { Provider } from "react-redux";
-import store, { persistor } from "../store/store";
-import { PersistGate } from "redux-persist/integration/react";
+import type { AppProps } from 'next/app'
+import Script from 'next/script'
+import { AuthUserProvider } from '../contexts/auth.context'
+import { Provider } from 'react-redux'
+import store, { persistor } from '../store/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -15,43 +15,45 @@ export default function App({ Component, pageProps }: AppProps) {
       <div>
         <Script
           src="/assets/js/jquery.js"
-          onLoad={() => console.log("jQuery loaded")}
+          onLoad={() => console.log('jQuery loaded')}
         />
         <Script
           src="/assets/js/fontawesome.min.js"
-          onLoad={() => console.log("fontAwesome loaded")}
+          onLoad={() => console.log('fontAwesome loaded')}
         />
         <Script
           src="/assets/js/waypoints.min.js"
-          onLoad={() => console.log("waypoint loaded")}
+          onLoad={() => console.log('waypoint loaded')}
         />
         <Script
-          src="/assets/js/bootstrap.min.js"
-          onLoad={() => console.log("bootsrap loaded")}
+          src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
+          integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+          crossOrigin="anonymous"
+          onLoad={() => console.log('bootsrap loaded')}
         />
         <Script
           src="/assets/js/swiper.min.js"
-          onLoad={() => console.log("swiper loaded")}
+          onLoad={() => console.log('swiper loaded')}
         />
         <Script
           src="/assets/js/jquery.countdown.min.js"
-          onLoad={() => console.log("jquery countdowm loaded")}
+          onLoad={() => console.log('jquery countdowm loaded')}
         />
         <Script
           src="/assets/js/jquery.counterup.min.js"
-          onLoad={() => console.log("countup loaded")}
+          onLoad={() => console.log('countup loaded')}
         />
         <Script
           src="/assets/js/isotope.pkgd.min.js"
-          onLoad={() => console.log("isotope loaded")}
+          onLoad={() => console.log('isotope loaded')}
         />
         <Script
           src="/assets/js/lightcase.js"
-          onLoad={() => console.log("lightcase loaded")}
+          onLoad={() => console.log('lightcase loaded')}
         />
         <Script
           src="/assets/js/functions.js"
-          onLoad={() => console.log("function loaded")}
+          onLoad={() => console.log('function loaded')}
         />
         <Provider store={store}>
           <PersistGate loading={null} persistor={persistor}>
@@ -60,5 +62,5 @@ export default function App({ Component, pageProps }: AppProps) {
         </Provider>
       </div>
     </AuthUserProvider>
-  );
+  )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,21 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <div id="modal-root"></div>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
